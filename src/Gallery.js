3e5c581b3e57fc57997ec6ab306f17077af2b429
Rahul sharma <120250301+Rahulsharma0223@users.noreply.github.com>
2024-09-24 22:19:13 +0530

    completed basic figma design

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
 import './Gallery.css';
 import bgImage from './images/bg.jpg'; // Path to your background image
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
+import image4 from './images/image4.jpg';
+import image5 from './images/image5.jpg';
+import image6 from './images/image6.jpg';
+import image7 from './images/image7.jpg';
+import image8 from './images/image8.jpg';
+import image9 from './images/image9.jpg';
+import image10 from './images/image10.jpg';
+import image11 from './images/image11.jpg';
+import image12 from './images/image12.jpg';
 
 const Gallery = () => {
   const [currentSlide1, setCurrentSlide1] = useState(0);
   const [currentSlide2, setCurrentSlide2] = useState(0);
 
   const imagesSet1 = [
-    "image1.jpg", "image2.jpg", "image3.jpg",
-    "image4.jpg", "image5.jpg", "image6.jpg"
+    image1, image2, image3,
+    image4, image5, image6
   ];
 
   const imagesSet2 = [
-    "image7.jpg", "image8.jpg", "image9.jpg",
-    "image10.jpg", "image11.jpg", "image12.jpg"
+    image7, image8, image9,
+    image10, image11, image12
   ];
 
   const handleNext1 = () => {
